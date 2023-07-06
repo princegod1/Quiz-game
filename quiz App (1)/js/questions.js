edc41a4223ee5d7967af4cdb7cc4aabdce9c14de
princegod1 <95476999+princegod1@users.noreply.github.com>
2023-07-06 22:02:10 +0300

    Add files via upload

---
+++ quiz App (1)/js/questions.js
@@ -0,0 +1,26 @@
+let MCQS = [{
+    question: "What does Hagar mean?",
+    choice1: "meditate",
+    choice2: "pray",
+    choice3: "fire",
+    choice4: "sing",
+    answer: 0
+},
+           {
+    question: "Who is the Secretary General of LW Inc. ?",
+    choice1: "Pastor Lanra Alabi",
+    choice2: "Pastor Biodun Lawal",
+    choice3: "Pastor Kay Adesina",
+    choice4: "Pastor T.T. Edun",
+    answer: 2
+           },
+
+    
+            {
+               question: "Who is the Asst. Secretary General of LW Inc. ?",
+               choice1: "Pastor Lanra Alabi",
+               choice2: "Pastor Biodun Lawal",
+               choice3: "Pastor Kay Adesina",
+               choice4: "Pastor T.T. Edun",
+               answer: 1
+}];
